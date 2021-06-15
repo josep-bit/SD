849b194aa26dda3191828a5337eb807dc5b5051c
josep-bit <85629370+josep-bit@users.noreply.github.com>
2021-06-15 20:27:22 +0200

    lastchange

--- rpc/client.py
+++ rpc/client.py
@@ -1,33 +1,45 @@
 import xmlrpc.client
 
-#number of files
-n_files = 0
+# nomber of workers
+n_workers = 0
+file = []
 
 class client:
     def __init__(self, code):
         self.code = code
 
     def input_file_and_create_worker(self):
-        global n_files
-        print("How many files do you want to enter?")
-        n_files = input()
+        global n_workers
+        print("How many workers do you want to enter?")
+        n_workers = input()
         print("Name of files with " ","":")
         file = input()
         f = file.split(",")
         return f
 
-
     def choose_routine(self):
         task = ''
+        print("If you want to createworker, write createW")
         print("If you want to countingWords, write countW")
         print("If you want to wordCount,  write wordC")
         print("If you want another function,  write the routine")
+        print("If you want to see workers, write listw")
+        print("If you want to eliminate workers, write eliw")
+        print("If you want to exit, write exit")
         routine = input()
+        if routine == "createW":
+            task = routine
         if routine == "countW":
             task = routine
         if routine == "wordC":
             task = routine
         if routine == "else":
+            task = routine
+        if routine == "listw":
+            task = routine
+        if routine == "eliw":
+            task = routine
+        if routine == "exit":
             task = routine
         return task
 
@@ -38,7 +50,25 @@ if __name__ == '__main__':
     code = input()
     Server.deleteclient(code)
     client = client(code)
-    file = client.input_file_and_create_worker()
-    Server.createnWorkers(n_files,code)
-    task = client.choose_routine()
-    Server.EnqueRedisTask(file,task,code)
+    menu = True
+    while menu:
+        task = client.choose_routine()
+        if task == "createW":
+            file = client.input_file_and_create_worker()
+            Server.createnWorkers(n_workers, code)
+        if task == "listw":
+            print(Server.list_worked())
+        if task == "eliw":
+            print("which is the id of worker to eliminate?")
+            id_worker = input()
+            Server.remove_worker(int(id_worker))
+            print(Server.list_worked())
+
+        if task == "countW":
+            Server.EnqueRedisTask(file, task, code)
+            print(Server.add_value(code))
+        if task == "wordC":
+            Server.EnqueRedisTask(file, task, code)
+            print(list(Server.has_dic(code)))
+        if task == "exit":
+            menu = False
--- rpc/master.py
+++ rpc/master.py
@@ -4,12 +4,12 @@ from multiprocessing import Process
 import logging
 from redis import Redis
 from routine import feature
+
 checkQueue = Redis(host='localhost', port=6379, db=0)
 
 # global var
 id_worker = 0
 dic = {}
-proc = Process
 
 
 class master:
@@ -23,67 +23,59 @@ class master:
     def ping(self):
         return True
 
-
-    def add_value(self,code):
+    def add_value(self, code):
         result = checkQueue.lrange(code, 0, -1)
         r = map(int, result)
-        add = reduce(lambda x,y: x+y,r)
-        print(add)
+        add = reduce(lambda x, y: x + y, r)
+        checkQueue.blpop(code,10)
+        return add
 
-    def has_dic(self,code):
+    def has_dic(self, code):
         result = checkQueue.lrange(code, 0, -1)
-        for i in result:
-            print(i, "\n")
+        checkQueue.blpop(code,10)
+        return str(result)
 
     def task_to_do(self, key):
-        while not quit():
+        while True:
             job = checkQueue.blpop(key, 10)
             result = eval(job)
             checkQueue.lpush(key, result)
 
-    def createnWorkers(self, n_files, key):
+    def createnWorkers(self, n_worker, key):
         i = 0
-        while i < int(n_files):
+        while i < int(n_worker):
             self.create_worker(key)
-            self.list_worked()
             i += 1
 
-    def EnqueRedisTask(self,list,task,key):
+    def EnqueRedisTask(self, list, task, key):
         f = feature.task1()
-        t = ''
         if task == 'countW':
             for i in list:
                 t = f.counting_Words(i)
-                self.EnqueRedis(key,t)
-            self.add_value(key)
-            self.remove_worker()
+                self.EnqueRedis(key, t)
         else:
             for i in list:
                 t = f.word_Count(i)
-                self.EnqueRedis(key,t)
-            self.has_dic(key)
-            self.remove_worker()
-
+                self.EnqueRedis(key, t)
 
     def create_worker(self, key):
-        global proc, id_worker, dic
+        global id_worker, dic
         proc = Process(target=self.task_to_do, args=(key,))
         proc.start()
         dic[id_worker] = proc
         id_worker += 1
 
     def list_worked(self):
-        i = 0
-        while i < id_worker:
-            print("Worker", dic[i])
-            i = i + 1
+        global dic
+        return str(dic)
 
-    def remove_worker(self):
-        proc.kill()
+    def remove_worker(self, id_worker):
+        dic[id_worker].terminate()
 
-    def deleteclient(self,code):
+    def deleteclient(self, code):
         if checkQueue.exists(code):
             checkQueue.delete(code)
+
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
